28481942a43d89ae4053e7948f415d2cf77a67b1
mabrao <mabrao99@outlook.com>
2024-02-11 19:50:57 +0000

    record password window

--- recordPassword.cpp
+++ recordPassword.cpp
@@ -12,7 +12,7 @@
 // constructor
 RecordPassword::RecordPassword()
     // there are size and style params here that can be passed to the wxFrame constructor
-    : wxFrame(nullptr, wxID_ANY, "Record Password")
+    : wxFrame(nullptr, wxID_ANY, "Record Password", wxPoint(POS_WINDOW_X,POS_WINDOW_Y), wxSize(SIZE_WINDOW_X,SIZE_WINDOW_Y))
 {
 
     wxMenu *menuFile = new wxMenu;
@@ -32,8 +32,29 @@ RecordPassword::RecordPassword()
         // Create a panel
     wxPanel* panel = new wxPanel(this, wxID_ANY);
 
+    // text box titles
+    wxStaticText* passwordTitle = new wxStaticText(panel, wxID_ANY, "Password title", wxPoint(POS_X_TEXT, START_POS_Y_TEXT+MID_TEXT_BOX_OFFSET), wxSize(SIZE_TEXT_X, SIZE_TEXT_Y));
+    wxStaticText* password = new wxStaticText(panel, wxID_ANY, "Enter password", wxPoint(POS_X_TEXT, START_POS_Y_TEXT+50+MID_TEXT_BOX_OFFSET), wxSize(SIZE_TEXT_X, SIZE_TEXT_Y));
+    wxStaticText* encryptionTitle = new wxStaticText(panel, wxID_ANY, "Encryption type", wxPoint(POS_X_TEXT, START_POS_Y_TEXT+100+MID_TEXT_BOX_OFFSET), wxSize(SIZE_TEXT_X, SIZE_TEXT_Y));
+    wxStaticText* fileNameTitle = new wxStaticText(panel, wxID_ANY, "File name", wxPoint(POS_X_TEXT, START_POS_Y_TEXT+150+MID_TEXT_BOX_OFFSET), wxSize(SIZE_TEXT_X, SIZE_TEXT_Y));
+
+    // text boxes
+    wxTextCtrl* tbPasswordTitle = new wxTextCtrl(panel, wxID_ANY, "", wxPoint(POS_X_BOX, START_POS_Y_BOX), wxSize(SIZE_BOX_X, SIZE_BOX_Y), wxTE_PROCESS_ENTER);
+    wxTextCtrl* tbPassword = new wxTextCtrl(panel, wxID_ANY, "", wxPoint(POS_X_BOX, START_POS_Y_BOX+50), wxSize(SIZE_BOX_X, SIZE_BOX_Y), wxTE_PASSWORD);
+    wxTextCtrl* tbEncryption = new wxTextCtrl(panel, wxID_ANY, "", wxPoint(POS_X_BOX, START_POS_Y_BOX+100), wxSize(SIZE_BOX_X, SIZE_BOX_Y), wxTE_PROCESS_ENTER);
+    wxTextCtrl* tbFileName = new wxTextCtrl(panel, wxID_ANY, "", wxPoint(POS_X_BOX, START_POS_Y_BOX+150), wxSize(SIZE_BOX_X, SIZE_BOX_Y), wxTE_PROCESS_ENTER);
+
+
+    // Set font for the static text (optional)
+    wxFont font(10, wxFONTFAMILY_ROMAN, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_SEMIBOLD);
+    passwordTitle->SetFont(font);
+    password->SetFont(font);
+    encryptionTitle->SetFont(font);
+    fileNameTitle->SetFont(font);
+
     // Create a backward button
-    wxButton* backButton = new wxButton(panel, wxID_BACKWARD, "Backward Button", wxPoint(10, 10), wxSize(150, 30));
+    wxButton* recordButton = new wxButton(panel, wxID_BACKWARD, "Record password", wxPoint(POS_X_BUTTONS, POS_Y_BUTTONS), wxSize(SIZE_BUTTONS_X, SIZE_BUTTONS_Y));
+    wxButton* backButton = new wxButton(panel, wxID_BACKWARD, "Back", wxPoint(POS_X_BUTTONS+200, POS_Y_BUTTONS), wxSize(SIZE_BUTTONS_X, SIZE_BUTTONS_Y));
 
     // Bind a function to handle button click events
     backButton->Bind(wxEVT_BUTTON, &RecordPassword::OnBackButtonClick, this);
--- recordPassword.hpp
+++ recordPassword.hpp
@@ -11,6 +11,38 @@
 
 #include <wx/wx.h>
 
+// macros with position and size for UI elements
+
+// window pos
+#define POS_WINDOW_X 100
+#define POS_WINDOW_Y 100
+// window size
+#define SIZE_WINDOW_X 500
+#define SIZE_WINDOW_Y 400
+
+#define MID_TEXT_BOX_OFFSET 6.25
+
+// pos for text titles
+#define POS_X_TEXT 100
+#define START_POS_Y_TEXT 50
+// size for text titles
+#define SIZE_TEXT_X 200
+#define SIZE_TEXT_Y 50
+
+//pos for text boxes
+#define POS_X_BOX 250
+#define START_POS_Y_BOX 50
+//size for text boxes
+#define SIZE_BOX_X 200
+#define SIZE_BOX_Y 25
+
+// pos for buttons
+#define POS_X_BUTTONS 75
+#define POS_Y_BUTTONS 275
+//size for buttons
+#define SIZE_BUTTONS_X 175
+#define SIZE_BUTTONS_Y 30
+
 class RecordPassword : public wxFrame
 {
 public:
